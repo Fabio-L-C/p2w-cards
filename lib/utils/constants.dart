8524ff37b4a9eb267ed95d52148df0c723899acd
Fabio Louzada Colodette <fabio.loutte@gmail.com>
2022-06-20 19:26:24 -0300

    Carregando produtos do firebase corrigindo erro

---
+++ lib/utils/constants.dart
@@ -0,0 +1,5 @@
+// ignore_for_file: constant_identifier_names
+
+const BASE_URL = 'https://p2w-cards-keys-default-rtdb.firebaseio.com/';
+const PRODUCTS_URL =
+    'https://p2w-cards-keys-default-rtdb.firebaseio.com/products.json';
